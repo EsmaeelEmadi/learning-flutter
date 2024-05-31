d216e9b684cbab422d86822b935bd6ed4b11ecfc
emad <emadi68moh@gmail.com>
2024-05-31 13:21:35 +0330

    feat: making bottomSheet transparent in order to solve the issue on adding custom colors

--- lib/main.dart
+++ lib/main.dart
@@ -43,6 +43,9 @@ class MyApp extends StatelessWidget {
                 debugShowCheckedModeBanner: false,
                 title: 'Flutter Demo',
                 theme: ThemeData(
+                    bottomSheetTheme: const BottomSheetThemeData(
+            surfaceTintColor:Colors.transparent,
+          ),
                   primarySwatch: Colors.lightBlue,
                   colorScheme: ColorScheme.fromSeed(
                     seedColor: Colors.blueAccent,
